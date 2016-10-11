a6735603311ce65edd472f2c3b80ffa628fcb7a1
simadad <simadad@sina.com>
2016-10-11 11:12:27 +0800

    selector

---
+++ english-selector/eng-selector.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import re
+mixFile = open('from.txt', 'r')
+mix = mixFile.read()
+words = re.findall(r'[a-zA-Z]+', mix)
+print (words)
+words.sort(key=str.lower)
+print (words)
