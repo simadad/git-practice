ada89bba86696255671bf9bea78def2bc06c85d4
simadad <simadad@sina.com>
2016-10-12 19:41:13 +0800

    optimize

--- red_packge/money.py
+++ red_packge/money.py
@@ -35,13 +35,26 @@ def on_aver(amount, quantity):
 
 
 def distribute(amount, quantity, average, part):
-    # 前 N-1 人按权重分配红包
+    # 分配红包
     accumulation = 0
     for i in range(quantity-1):
+        # 前N-1人红包分配
         part[i] = round(average*part[i], 2)
+        # 确保当前红包至少为0.01
+        if part[i] > 0:
+            pass
+        else:
+            part[i] = 0.01
+            # 从下一个红包中削减0.01的份额
+            part[i+1] -= 0.01/average
         # 红包累加
         accumulation += part[i]
-        print ('第 %d 人： %.2f 元' % (i+1, part[i]))
+        # 确保剩下的金额足够
+        if money-accumulation < 0.01*(quantity-1-i):
+            # 削减当前红包金额
+            part[i] -= 0.01*(quantity-1-i)-(money-accumulation)
+            accumulation -= 0.01*(quantity-1-i)-(money-accumulation)
+        print ('第 %d 人： %.2f 元' % (i + 1, part[i]))
     # 最后一份红包
     print ('第 %d 人： %.2f 元' % (quantity, amount-accumulation))
 
@@ -49,7 +62,7 @@ def distribute(amount, quantity, average, part):
 while True:
     money = numb('请输入红包总金额：', False)
     count = numb('请输入红包个数: ', True)
-    if count*0.01 < money:
+    if count*0.01 <= money:
         pass
     else:
         print ('单个红包不能低于0.01元\n')
