2394af0d29eb0f9c2c59b26b21ab47655611de8a
simadad <simadad@sina.com>
2016-10-18 12:36:45 +0800

    href

--- web/code.py
+++ web/code.py
@@ -3,7 +3,9 @@ import web
 
 urls = (                                                    # controller      URL 控制器
     '/', 'Index',
-    '/movie/(\d+)', 'Movie'
+    '/movie/(\d+)', 'Movie',
+    '/cast/(.*)', 'Cast',
+    '/director/(.*)', 'Director'
 )
 app = web.application(urls, globals())
 db = web.database(dbn='sqlite', db='MovieSite.db')          # #      调用数据库，获取信息
@@ -15,15 +17,15 @@ class Index:                                            # 控制函数 Index
         pass
 
     def GET(self):
+        print ('index_get')
         movies = db.select('movie')
-        print ('get')
         return render.index(movies)
 
     def POST(self):
+        print ('index_post')
         data = web.input()
-        condition = r'title like "%' + data.title + r'%"'
+        condition = r'title like "%' + data.title + r'%"'   # data.title ?????????
         movies = db.select('movie', where=condition)
-        print ('post')
         return render.index(movies)
 
 
@@ -32,24 +34,37 @@ class Movie:                                                # 控制函数 Movie
         pass
 
     def GET(self, movie_id):
-        print 1
-        movie_id = int(movie_id)
+        print ('movie_get')
         print movie_id
-        movie = db.select('movie', where='id=$movie_id', vars=locals())[0]
-        print movie
-        return render.movie(movie)
-'''
-movies = [
-    {
-        'title': 'Forrest Gump',
-        'year': 1994,
-    },
-    {
-        'title': 'Titanic',
-        'year':	1997,
-    },
-]
-'''
+        movie_id = int(movie_id)
+        movies = db.select('movie', where='id=$movie_id', vars=locals())[0]
+        return render.movie(movies)
+
+
+class Director:
+    def __init__(self):
+        pass
+
+    def GET(self, movie_director):
+        print ('director_get')
+        print (movie_director)
+        condition = r'directors like "%' + movie_director + r'%"'
+        movies = db.select('movie', where=condition)
+        return render.index(movies)
+
+
+class Cast:
+    def __init__(self):
+        pass
+
+    def GET(self, movie_cast):
+        print ('cast_get')
+        print (movie_cast)
+        condition = r'casts like "%' + movie_cast + r'%"'
+        movies = db.select('movie', where=condition)
+        return render.index(movies)
 
 if __name__ == "__main__":
     app.run()
+
+
