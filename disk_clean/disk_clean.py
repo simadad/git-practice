c20a5fdbb9633e4aba1b591757f18458ef3af4ab
simadad <simadad@sina.com>
2016-12-05 21:00:52 +0800

    aaa

--- disk_clean/disk_clean.py
+++ disk_clean/disk_clean.py
@@ -21,9 +21,6 @@ del_data = r'''
 %userprofile%\Temporary Internet Files\*.*
 %userprofile%\Temp\*.*
 '''
-del_dirs = re.findall(r'(?<=\\).+(?=\\)', del_data)
-del_files = re.findall(r'(?<=\.)\w+', del_data)
-# 正则环视匹配垃圾文件夹名及垃圾文件扩展名
 
 
 def get_size_and_del(gsd_root):
@@ -35,7 +32,7 @@ def get_size_and_del(gsd_root):
         os.remove(gsd_root)
         file_numb += 1
         size += t_size
-        print 'file：' + gsd_root + ' cleaned'
+        print 'file：' + gsd_root + ' removed'
     except WindowsError:
         try:
             # 格式不正确则移除文件夹
@@ -43,25 +40,30 @@ def get_size_and_del(gsd_root):
             os.rmdir(gsd_root)
             dir_numb += 1
             size += t_size
-            print 'dir：' + gsd_root + ' cleaned'
+            print 'dir：' + gsd_root + ' removed'
         except WindowsError:
             pass
             # 文件夹不为空则跳过
 
-
+dir_numb, file_numb, size = 0, 0, 0
+# 初始化统计数据
 root_sys_drive = os.environ['systemdrive'] + '\\'
 # 获取系统盘
 root_user_profile = os.environ['userprofile']
 # 获取用户目录
-dir_numb = 0
-file_numb = 0
-size = 0
-# 初始化统计数据
+root_win_dir = os.environ['windir']
+# 获取 Windows 目录
+del_files = re.findall(r'(?<=\.)\w+', del_data)
+# 匹配垃圾文件扩展名
+temp_del_userprofile = re.findall(r'(?<=%userprofile%\\).+(?=\\)', del_data)
+# 获取用户文目录下垃圾文件夹名
+del_userprofile = map(lambda temp_dir: os.path.join(root_user_profile, temp_dir), temp_del_userprofile)
+# 组合用户文目录下垃圾文件夹全目录名
+temp_del_windir = re.findall(r'(?<=%windir%\\).+(?=\\)', del_data)
+del_windir = map(lambda temp_dir: os.path.join(root_win_dir, temp_dir), temp_del_windir)
+
 for roots, dirs, files in os.walk(root_sys_drive, topdown=False):
     # 生成并展开以 root 为根目录的目录树，参数 topdown 设定展开方式从底层到顶层
-    print 'roots：' + roots
-    dir_to_del = re.findall(r'(?<=\\)[^\\]*$', roots)[0]
-    # 逆序肯定环视，匹配根目录
     for every_dir in dirs:
         full_path = os.path.join(roots, every_dir)
         # 组合文件夹完整路径
@@ -72,9 +74,16 @@ for roots, dirs, files in os.walk(root_sys_drive, topdown=False):
             file_extension = re.findall(r'(?<=\.)\w+$', every_file)[0]
             # 逆序肯定环视，匹配每个文件的扩展名
         except IndexError:
-            # 排除无扩展名文件正则匹配报错
+            # 排除无扩展名文件正则匹配时报错
             file_extension = ''
-        if dir_to_del in del_dirs and root_user_profile in roots or file_extension in del_files:
+        condition = False
+        for i in range(len(del_userprofile)):
+            condition += del_userprofile[i] in roots
+        for i in range(len(del_windir)):
+            condition += del_windir[i] in roots
+        condition += file_extension in del_files
+        # 组合判断条件
+        if condition:
             full_path = os.path.join(roots, every_file)
             # 组合文件完整路径
             get_size_and_del(full_path)
